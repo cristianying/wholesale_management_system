e68c7dd223ad8047ad9370bb1c4d42169b01f103
Cristian Ying <cristianying@hotmail.com>
2023-09-12 18:20:03 +0800

    update url

--- react-admin/src/apis/Warehousedb.js
+++ react-admin/src/apis/Warehousedb.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:9000",
-  baseURL: "https://wholesalemanagementsystem-production.up.railway.app/",
+  baseURL: "https://wholesalemanagementsystem-production.up.railway.app",
 });
